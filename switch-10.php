c62dba2de761eac46fdf7e9e82d431999a9fb78b
LobitoM365 <almariojuancho@gmail.com>
2023-05-18 16:48:37 -0500

    d

--- switch-10.php
+++ switch-10.php
@@ -2,11 +2,11 @@
 $numero = 3;
 
 $resultado = match (true){
-    $numero === 1 => "",
-    $numero === 2 => "",
-    $numero === 3 => "",
-    $numero === 4 => "",
-    $numero === 5 => ""
+    $numero === 1 => "adios",
+    $numero === 2 => "buena madrugada",
+    $numero === 3 => "hola buenas tardes",
+    $numero === 4 => "hola buenas noche",
+    $numero === 5 => "hola buenos dias"
 };
 
 echo $resultado;
